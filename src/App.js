ac83a16001521ea748733877253108f4f760da62
Davi Guilherme <job.davic@gmail.com>
2022-01-12 18:33:15 -0300

    Fisished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import img from './assets/Cartaz.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div className="img_container" >
+      <img className="img" src={img} alt="Logo" />
+        <h1 className="text_maya">Maya Angelou</h1>
+      </div>
+      <div className="Text"> 
+        <span className="pink">*</span>
+        <h1>Escritora, Poeta e Ativista </h1>
+        <span className="pink">*</span>
+      </div>
+      <div className="Text">
+        <h3>4 de abril de 1928 - 28 de maio de 2014</h3>
+      </div>
+      <div className="white">
+        <p>Maya Angelou, nascida Marguerite Annie Johnson, é mais conhecida por sua grande autobiografia : "I Know Why the Caged Bird Sings" (Eu Sei Porque os Pássaros Engaiolados Cantam), publicada em 1969. Ela publicou seis outros livros autobiográficos desde então, com o volume final lançado em 2002. Ela foi uma poeta prolífica e seus poemas eram muitas vezes sobre a experiência de mulheres afro-americanas, o espírito humano e a justiça social para todos.</p>
+      </div>
+      <div className = "Text"><span className="pink">*</span><h1>Você sabia?</h1><span className="pink">*</span></div>
+      <div className="card_container">
+        <div className="card white"><p>Como ativista, ela ajudou a organizar e produzir eventos de arrecadação históricos para lutas de direitos civis nas décadas de 50 e 60. As lutas de afro-americanos também se refletiram em sua prolífica carreira de escritora.</p></div>
+        <div className="card white"><p>Como autora, sua obra se estendeu à escrita para o cinema, televisão e teatro. Influenciada por tradições orais afro-americanas , apresentou sua poesia através de leituras públicas, gravações de áudio e documentários.</p></div>
+        <div className="card white"><p>Seu nome foi cunhado pelo seu irmão mais velho Bailey Jr. Ele estava tentando dizer "minha irmã" (my sister) quando ela era um bebê, mas acabou dizendo "maya sister".</p></div>
+      </div>
+      <div className="footer_container">
+        <div className="white footer_link"><a>https://www.poetryfoundation.org/poets/maya-angelou</a></div>
+        <div className="white footer_link"><a>https://www.ducksters.com/biography/authors/mayaangelou.php</a></div>
+      </div>
     </div>
+
   );
 }
 
